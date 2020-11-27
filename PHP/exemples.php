5156b9d88d729532e2f9e18508886caebe97e347
FirasFendri <firas.fendri@enis.tn>
2020-11-27 15:10:15 +0100

    exetnsions php

--- PHP/exemples.php
+++ PHP/exemples.php
@@ -1,14 +1,38 @@
 <?php
 class voiture
 {
-    private $nb_tour;
+    private $nb_tour;//to generate auto getter and setters just type cntrl+P and then type >PHP getter and setters
     private $rayon_roue;
+    protected $new;//to auto generate constructor and typr cntrl+p and the >constructor
 
-    function __construct()
+    protected $new2;
+
+    public function __construct($new, $new2)
     {
+        $this->new = $new;
+        $this->new2 = $new2;
     }
-    function __construct()
+
+
+
+
+    /**
+     * Get the value of nb_tour
+     */ 
+    public function getNb_tour()
     {
-        
+        return $this->nb_tour;
+    }
+
+    /**
+     * Set the value of nb_tour
+     *
+     * @return  self
+     */ 
+    public function setNb_tour($nb_tour)
+    {
+        $this->nb_tour = $nb_tour;
+
+        return $this;
     }
 }
